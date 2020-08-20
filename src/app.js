40aa57e8feeb166fddca65aa39cfd29169ea7d4c
MarcoMadeddu <marcomade99@gmail.com>
2020-08-20 19:23:55 +0200

    complete header and skill section

--- src/app.js
+++ src/app.js
@@ -1,28 +1,57 @@
 const $ = require( "jquery" );
 require('bootstrap');
 require('animate.css');
-require('slick-carousel');
 
 
+$( document ).ready(function() {
+  var logo = $('.logo');
+  var profile = $('.img');
+  var links= $('.link');
+  console.log(links);
 
-var lor = "ciao";
+  links.click(function(){
+    var current = $(this);
+    current.addClass('animate__zoomOut');
+     //interval to change animation
+    var change = setTimeout(ActiveLink, 1500);
+    function ActiveLink(){
+    console.log(current);
+    current.removeClass('animate__zoomOut');
+    current.addClass('animate__zoomIn');
+    setTimeout(function(){links.removeClass('animate__zoomIn , animate__fadeInUpBig');}, 1000);
+  }
+  })
+  
+  
 
+  //scroll to effect
+  $(window).scroll(function(){
+    //scroll on header
+    links.toggleClass('animate__fadeOutUpBig', $(this).scrollTop() > 300);
+    if(!links.hasClass('animate__fadeInUpBig')){
+      links.toggleClass('animate__fadeInUpBig', $(this).scrollTop() > 300);
+    }
+    //scroll on skill section
+    $(".a").toggleClass('animate__backInLeft', $(this).scrollTop() > 250);
+    $(".title").toggleClass('animate__bounce', $(this).scrollTop() > 250);
+    $(".skill-icon").toggleClass('animate__rotateIn', $(this).scrollTop() > 250);
+    $(".skill-icon-left").toggleClass('animate__lightSpeedInLeft', $(this).scrollTop() > 250);
+    $(".skill-icon-right").toggleClass('animate__lightSpeedInRight', $(this).scrollTop() > 250);
+    $(".skill-text").toggleClass('animate__bounceIn', $(this).scrollTop() > 250);
+    $(".adv").toggleClass('animate__backInUp', $(this).scrollTop() > 250);
 
+    //scroll on Work section
+    $("#ban1").toggleClass('animate__rotateIn', $(this).scrollTop() > 250);
+    $("#ban2").toggleClass('animate__lightSpeedInLeft', $(this).scrollTop() > 250);
+    $("#ban3").toggleClass('animate__lightSpeedInRight', $(this).scrollTop() > 250);
+  })
 
-
-
-$(window).scroll(function(){
-    $(".a").toggleClass('animate__backInLeft', $(this).scrollTop() > 150)
+  //interval to change logo
+  setInterval(changeLogo, 5000);
+  function changeLogo(){
+    profile.toggleClass('animate__flipOutY');
+    profile.toggleClass('off');
+    logo.toggleClass('off');
+    logo.toggleClass('animate__flipInY');
+  }
 })
-
-// S W I P E R
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
